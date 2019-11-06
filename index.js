f5af3a02be7c5857c8d638e91b8c077714b276e8
akemioshiro <akemioshiro@hotmail.com>
2019-11-05 22:30:32 -0300

    Inclusão de index.js e index.html

---
+++ index.js
@@ -0,0 +1,59 @@
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+
+const options = {
+  uri: 'https://app1-paoshiro.s3-sa-east-1.amazonaws.com/index.html',
+  transform: function (body) {
+    return cheerio.load(body)
+  }
+}
+
+rp(options)
+.then(($) => {
+
+  const listaRivalidade = []
+  $('.table tr').each(function() {
+    const rivalidade = {
+      posicao: $(this).find('.titlePosition').text(),
+      time1: $(this).find('.titleTeam1').text(),
+      time2: $(this).find('.titleTeam2').text(),
+      pontuacao: $(this).find('.titlePoints').text(),
+    }
+    console.log("Rivalidade entre times: ", JSON.stringify(rivalidade));
+
+    if(rivalidade.nome !== "")
+      listaRivalidade.push(rivalidade);
+  });
+
+  verificaRivalidadeCorinthiana(listaRivalidade);
+
+})
+.catch((err) => {
+  console.log("Erro de captura de informação", err);
+});
+
+function verificaRivalidadeCorinthiana(listaRivalidade){
+  let rivalidadeCorinthiansPalmeiras;
+  let rvalidadeCorinthiansSantos;
+
+  listaRivalidade.forEach(element => {
+    if(element.time1 === 'Corinthians' || element.time2 === 'Corinthians'){
+      if(element.time1== 'Palmeiras' || element.time2 === 'Palmeiras'){
+        rivalidadeCorinthiansPalmeiras = element;
+      }
+    }
+
+    if(element.time1 === 'Corinthians' || element.time2 === 'Corinthians'){
+      if(element.time1== 'Santos' || element.time2 === 'Santos'){
+        rvalidadeCorinthiansSantos = element;
+      }
+    }
+  });
+
+  if((rivalidadeCorinthiansPalmeiras && rivalidadeCorinthiansPalmeiras.pontuacao) 
+    > (rvalidadeCorinthiansSantos && rvalidadeCorinthiansSantos.pontuacao)){
+      console.log("Validação da hipótese: ");
+      console.log(` ${rivalidadeCorinthiansPalmeiras.time1} X ${rivalidadeCorinthiansPalmeiras.time2} : ${rivalidadeCorinthiansPalmeiras.pontuacao}`);
+      console.log(` ${rvalidadeCorinthiansSantos.time1} X ${rvalidadeCorinthiansSantos.time2} : ${rvalidadeCorinthiansSantos.pontuacao}`);
+  }
+}
