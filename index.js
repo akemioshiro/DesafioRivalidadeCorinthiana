7d405a2b8021e2cea1487beea45b56df647943bd
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2019-11-07 21:03:11 -0300

    inclusão de comentários no código

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const rp = require('request-promise')
 const cheerio = require('cheerio')
 
+const CORINTHIANS = "Corinthians";
+const PALMEIRAS =  "Palmeiras";
+const SANTOS = "Santos";
+
+// A url: https://app1-paoshiro.s3-sa-east-1.amazonaws.com/index.html é uma página desenvolvida com base na página do globo esporte
 const options = {
   uri: 'https://app1-paoshiro.s3-sa-east-1.amazonaws.com/index.html',
   transform: function (body) {
@@ -12,7 +17,11 @@ rp(options)
   .then(($) => {
 
     const listaRivalidade = []
+
+    // Percorre as linhas das tabelas que contém os dados de cada clássico
     $('.table tr').each(function () {
+
+      // cria um objeto com informações dos maiores clássicos do futebol e de acordo com a classe da coluna da tabela
       const rivalidade = {
         posicao: $(this).find('.titlePosition').text(),
         time1: $(this).find('.titleTeam1').text(),
@@ -20,13 +29,15 @@ rp(options)
         pontuacao: $(this).find('.titlePoints').text(),
       }
 
-      if (rivalidade.time1 !== "") {
+      // verifico se é um objeto com dados válidos e incremento na lista
+      if (rivalidade.time1 && rivalidade.time2 && rivalidade.pontuacao) {
         console.log(` Rivalidade entre times: ${rivalidade.posicao} -  ${rivalidade.time1} X ${rivalidade.time2} : ${rivalidade.pontuacao}`);
         listaRivalidade.push(rivalidade);
       }
 
     });
 
+    // chamo o método responsável pela validação do grau de rivalidade
     verificaRivalidadeCorinthiana(listaRivalidade);
 
   })
@@ -34,28 +45,31 @@ rp(options)
     console.log("Erro de captura de informação", err);
   });
 
+
+  
+// Método que verifica a hipótese da rivalidade entre Corinthians X Palmeiras ser maior que Corinthians X Santos
 function verificaRivalidadeCorinthiana(listaRivalidade) {
   let rivalidadeCorinthiansPalmeiras;
-  let rvalidadeCorinthiansSantos;
+  let rivalidadeCorinthiansSantos;
 
   listaRivalidade.forEach(element => {
-    if (element.time1 === 'Corinthians' || element.time2 === 'Corinthians') {
-      if (element.time1 == 'Palmeiras' || element.time2 === 'Palmeiras') {
+    if (element.time1 === CORINTHIANS || element.time2 === CORINTHIANS) {
+      if (element.time1 == PALMEIRAS || element.time2 === PALMEIRAS) {
         rivalidadeCorinthiansPalmeiras = element;
       }
     }
 
-    if (element.time1 === 'Corinthians' || element.time2 === 'Corinthians') {
-      if (element.time1 == 'Santos' || element.time2 === 'Santos') {
-        rvalidadeCorinthiansSantos = element;
+    if (element.time1 === CORINTHIANS || element.time2 === CORINTHIANS) {
+      if (element.time1 == SANTOS || element.time2 === SANTOS) {
+        rivalidadeCorinthiansSantos = element;
       }
     }
   });
 
   if ((rivalidadeCorinthiansPalmeiras && rivalidadeCorinthiansPalmeiras.pontuacao)
-    > (rvalidadeCorinthiansSantos && rvalidadeCorinthiansSantos.pontuacao)) {
+    > (rivalidadeCorinthiansSantos && rivalidadeCorinthiansSantos.pontuacao)) {
     console.log("Validação da hipótese: ");
     console.log(` ${rivalidadeCorinthiansPalmeiras.time1} X ${rivalidadeCorinthiansPalmeiras.time2} : ${rivalidadeCorinthiansPalmeiras.pontuacao}`);
-    console.log(` ${rvalidadeCorinthiansSantos.time1} X ${rvalidadeCorinthiansSantos.time2} : ${rvalidadeCorinthiansSantos.pontuacao}`);
+    console.log(` ${rivalidadeCorinthiansSantos.time1} X ${rivalidadeCorinthiansSantos.time2} : ${rivalidadeCorinthiansSantos.pontuacao}`);
   }
 }
